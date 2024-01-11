62a291ec7fcf0291ea802319e6469a8ae15ee227
Estefap12 <estefa991235@gmail.com>
2024-01-11 15:05:21 -0500

    nt1 se implementa algoritmo de datos primitivos y suma desde consola

---
+++ datos.py
@@ -0,0 +1,4 @@
+numeroUno=int(input("Digita un numero entero: "))
+numeroDos=int(input("Digita el segundo numero entero: "))
+sumatoria=numeroUno+numeroDos
+print(f"la suma de {numeroUno} + {numeroDos}= {sumatoria}")
---
+++ main.py
@@ -0,0 +1,12 @@
+#Variables primitivas con Python
+#Quemar las variables, no es la mas indicada
+nombreEstudiante="Estefania Perez"
+edadEstudiante="24"
+estaturaEstudiante=1.55
+hinchaDelMejorEquipo=True
+
+#Pedir datos por teclado
+comidaFavarita=input("Digita tu comida Favorita: ")
+
+#Mostrar datos en pantalla (Salidas)
+print(f"Hola {nombreEstudiante} tu comida favorita es {comidaFavarita}")
